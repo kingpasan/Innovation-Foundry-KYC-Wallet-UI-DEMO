33896a7575ab9e796663e18912fab0bd39dbbc5e
Pasan Semage <pmsemage@gmail.com>
2018-10-15 10:50:33 +0530

    Sasindu Part - FingerPrintScanActivity - PasscodeActivity

--- app/src/main/java/lk/dialog/kyc/kycwallet/PasscodeActivity.java
+++ app/src/main/java/lk/dialog/kyc/kycwallet/PasscodeActivity.java
@@ -1,13 +1,43 @@
 package lk.dialog.kyc.kycwallet;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.hanks.passcodeview.PasscodeView;
 
 public class PasscodeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_passcode);
+
+        PasscodeView passcodeView = (PasscodeView) findViewById(R.id.passcodeView);
+        passcodeView.setListener(new PasscodeView.PasscodeViewListener() {
+            @Override
+            public void onFail() {
+                Toast.makeText(getApplication(), "Passcode Registration Failed.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(String number) {
+                Toast.makeText(getApplication(), "Passcode Registered Successfully.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(PasscodeActivity.this, PersonActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
