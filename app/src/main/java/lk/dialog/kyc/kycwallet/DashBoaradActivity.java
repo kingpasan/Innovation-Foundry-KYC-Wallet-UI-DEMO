9d6478ca0894dde203c74ec4cd5c138aba5eb176
Nidura Prageeth <niduraprageeth@gmail.com>
2018-10-16 09:48:16 +0530

    user profile UI create

--- app/src/main/java/lk/dialog/kyc/kycwallet/DashBoaradActivity.java
+++ app/src/main/java/lk/dialog/kyc/kycwallet/DashBoaradActivity.java
@@ -1,14 +1,36 @@
 package lk.dialog.kyc.kycwallet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class DashBoaradActivity extends AppCompatActivity {
+
+    private Button btnProfile;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        init();
+
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashBoaradActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    private void init() {
+        btnProfile = (Button) findViewById(R.id.btnProfile);
     }
 }
